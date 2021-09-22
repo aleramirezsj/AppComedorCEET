f3bed92cc0c048016905d6bc23cc23b36d367094
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2021-09-22 13:45:49 -0300

    Agregar archivos de proyecto.

---
+++ AppComedorCEET/AppComedorCEET/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AppComedorCEET.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppComedorCEET
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TipoDeRegistro();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
